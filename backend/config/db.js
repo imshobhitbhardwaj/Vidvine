752daad1a21390a7c99e72bcff062715153f30f4
Shobhit Bhardwaj <imshobhitbhardwaj@gmail.com>
2026-06-23 23:58:07 +0530

    Remove generated video files

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,16 @@
-// backend/config/db.js
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/videoStreamingDB");
+    await mongoose.connect(process.env.MONGO_URI);
+
     console.log("MongoDB connected");
   } catch (err) {
-    console.error("MongoDB connection error:", err.message);
+    console.error(
+      "MongoDB connection error:",
+      err.message
+    );
+
     process.exit(1);
   }
 };
